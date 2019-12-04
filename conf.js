05daa630758c04528fd2adf8d3e35c381edf90b2
Taco QA <58486912+tacoqa@users.noreply.github.com>
2019-12-04 17:36:44 -0500

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,6 @@
+// windows config
+exports.config = {
+  chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_77.0.3865.40.exe',
+  directConnect: true,
+  specs: ['**/todo-spec.js']
+};
